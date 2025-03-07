c4987e8042d56263f7e4d2c4e0002bad0ed92da1
hi-hyunjung <nhannhathegreat@gmail.com>
2025-03-07 18:09:02 +0900

    [Feature: main.ts 수정] * Detailed Notes:   * httpexception filter 필요함

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initializeTransactionalContext } from 'typeorm-transactional';
+import {
+  FastifyAdapter,
+  type NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import multiPart from '@fastify/multipart';
+import { ValidationPipe } from '@nestjs/common';
+// import { HttpExceptionFilter } from './common/filters';
+
+const globalPrefix = 'api';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  initializeTransactionalContext();
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({}),
+    { bufferLogs: true },
+  );
+
+  await app.register(multiPart);
+
+  app.enableCors({ origin: '*', exposedHeaders: ['Content-Disposition'] });
+
+  app.setGlobalPrefix(globalPrefix, { exclude: ['/docs/redoc'] });
+
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  await app.listen(process.env.PORT ?? 3003);
 }
 bootstrap();
